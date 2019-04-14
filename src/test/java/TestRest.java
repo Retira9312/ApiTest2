71ec51e1c8347af9b59ebe433ea94f97056c7034
Retira9312 <48682202+Retira9312@users.noreply.github.com>
2019-04-14 20:55:18 +0700

    Add files via upload

--- src/test/java/TestRest.java
+++ src/test/java/TestRest.java
@@ -16,48 +16,27 @@ import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.containsString;
 
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
 
 
 
-class response{
-
-}
 
 public class TestRest {
 
 
-    // String dom = "api.vk.com/method/";
-    // String metod = "users.get?";
-
-    // String id =  "user_id=20723173&";
-    //  String token =  "access_token=ad330c9d89a6c54375392821838ac72047261eec0de44523cf422e8aa21202a0a69579b01f46449866c72&";
-    // String ver =  "v=5.92";
     @Test(description = "API GET REQUEST TEST")
 
     public <string> void getRequestExampleTest() throws JSONException {
-        //  RequestSpecification httpRequest = given();
-        //  Response response = httpRequest.get( "https://" + dom + metod + id + token + ver + '"');
-        //  response.prettyPrint();
 
-        // Response responseF = httpRequest.get("https://api.vk.com/method/friends.get?user_id=20723173&access_token=ad330c9d89a6c54375392821838ac72047261eec0de44523cf422e8aa21202a0a69579b01f46449866c72&v=5.92");
-        //responseF.prettyPrint();
-
-
-        //Response responseS = httpRequest.get("https://api.vk.com/method/status.set?group_id=117179357&text='text'&access_token=39bad999230de9ed0e203e93c260db123f9b7fc5bfaca8baf2d360ae603f74af4f60a6d3a01b5ff41dcb0&v=5.92");
-        // responseS.prettyPrint();
-
-        //"https://api.vk.com/method/users.get?user_id=20723173&access_token=ad330c9d89a6c54375392821838ac72047261eec0de44523cf422e8aa21202a0a69579b01f46449866c72&v=5.92"
-        //private String response ;
 
         String CONEXT_PATH = "https://api.vk.com/method";
         Response response = given()
                 .accept(JSON)
                 .queryParam("user_id", "20723173")
-                .queryParam("access_token", "ad330c9d89a6c54375392821838ac72047261eec0de44523cf422e8aa21202a0a69579b01f46449866c72")
+                .queryParam("access_token", "1cfd05e145ee99f82b20ebf796cc07f247accb38e2a79c10baf60f8851cb27ea2a351cb739b4ab3e5c725")
                 .queryParam("v", "5.92")
                 .when()
                 .get(CONEXT_PATH + "/users.get")
-                //.when().get("https://" + dom + metod + id + token + ver + '"').
                 .then()
                 .statusCode(200)
                 .contentType(JSON)
@@ -65,16 +44,21 @@ public class TestRest {
                 .response();
 
         String jsonBody = response.getBody().asString();
+        System.out.println(jsonBody);
+
+
 
         try {
             JSONArray usersArray = new JSONArray(jsonBody);
             assertNotNull(usersArray);
             assertTrue(usersArray.length() > 0);
-        } catch (JSONException ex) {
-            
-            Assert.fail(ex.getLocalizedMessage());
+            assertTrue(usersArray.);
+        } catch (Exception ex) {
 
-       }
+            System.out.println(ex.getLocalizedMessage());
+
+        }
+        //System.out.println(usersArray);
         //.body(containsString("response"));
         // System.out.println(response);
 
